b08a97bc2c774f67e8f380891b860bfac05d03a8
rmdm <rmdm@users.noreply.github.com>
2015-05-31 23:12:37 +0400

    Fix scope immutability.

--- repository/example_extension/index.js
+++ repository/example_extension/index.js
@@ -1,6 +1,6 @@
 module.exports = ['example_extensible:', function (extensible) {
 
-    extensible.values = ['a', '\n', 'b', '\n', 'c']
+    extensible.values.push.apply(extensible.values, ['a', '\n', 'b', '\n', 'c'])
     extensible.start()
 
 }]
